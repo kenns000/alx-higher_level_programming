287966e289c925a1b5d37414ba9106eda57a7d57
Koranteng <e.s.koranteng@gmail.com>
2024-03-13 19:56:51 -0700

    Task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    '''prints the last digit of a number'''
+    LastDigit = abs(number) % 10
+    print(f"{last_digit}", end='')
+    return LastDigit
