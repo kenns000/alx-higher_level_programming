7172c04a67426edbe8aebf11583ee2fe25330b61
Koranteng Emmanuel Saforo <kenns@kenns.kenns.net>
2024-09-14 17:54:07 +0000

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for i in range(x):
+    for k in range(x):
         try:
-            print(my_list[i], end="")
+            print(my_list[k], end="")
             num += 1
         except IndexError:
             break
-    print("")
-    Return
-
-
+    print()
+    return num
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
+    i = 0
+    for k in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
+            print("{:d}".format(my_list[k]), end="")
+            i += 1
+        except(ValueError, TypeError):
+            continue
     print()
-    Return
-
-
+    return i
